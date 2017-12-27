8f00381329f439b07312fab6ae2d66f45cddd3c1
Steven J Raaen <stevenjraaen@Stevens-MacBook-Pro.local>
2017-12-27 15:27:54 -0500

    fixed train route

--- server.js
+++ server.js
@@ -11,71 +11,65 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 mongoose.connect('mongodb://heroku_dd83dt7l:1qcrmml3h73rgcgdjing0s868u@ds159776.mlab.com:59776/heroku_dd83dt7l', {
-  useMongoClient: true,
+    useMongoClient: true,
 }).then(function() {
-  console.log('Mongo connected via mongoose')
+    console.log('Mongo connected via mongoose')
 });
 var mta = new Mta({
-  key: 'd95f1fb11f498729369198ba2d321657', // only needed for mta.schedule() method                 
-        });
+    key: 'd95f1fb11f498729369198ba2d321657', // only needed for mta.schedule() method                 
+});
 
 app.get("/api/stops/:coordinates?", function(req, res) {
-/*    console.log(parseFloat(req.query.coordinates))
-    var lat = parseFloat(req.query.coordinates[1]).toFixed(6)
-    var lng = parseFloat(req.query.coordinates[0]).toFixed(6)*/
+    /*    console.log(parseFloat(req.query.coordinates))
+        var lat = parseFloat(req.query.coordinates[1]).toFixed(6)
+        var lng = parseFloat(req.query.coordinates[0]).toFixed(6)*/
 
-   /* if(req.query.coordinates) {*/
-Subways.aggregate([{
-    $geoNear: {
-        near: {
-            /*type: "Point",*/
-            coordinates: [-73.989951, 40.734673]
-        },
-        spherical: true,
-        distanceField: 'distance.dist',
-        distanceMultiplier: 0.00062,
-        num: 10 }
-}],
-function(error, doc) {
-    if (error) {
-        console.log(error);
-    } else {
-        /*console.log(doc)*/
-        res.json(doc);
-    }
-})
-/*} */
+    /* if(req.query.coordinates) {*/
+    Subways.aggregate([{
+            $geoNear: {
+                near: {
+                    /*type: "Point",*/
+                    coordinates: [-73.989951, 40.734673]
+                },
+                spherical: true,
+                distanceField: 'distance.dist',
+                distanceMultiplier: 0.00062,
+                num: 10
+            }
+        }],
+        function(error, doc) {
+            if (error) {
+                console.log(error);
+            } else {
+                /*console.log(doc)*/
+                res.json(doc);
+            }
+        })
+    /*} */
 });
-app.get('/api/status', function(req, res){
-    mta.status().then(function (doc) {
-  console.log(doc);
-  res.json(doc)
-});
+app.get('/api/status', function(req, res) {
+    mta.status().then(function(doc) {
+        console.log(doc);
+        res.json(doc)
+    });
 
 })
 // ------- route for selected train's schedule --------
 app.get("/api/train", function(req, res) {
-var feed= 21;
-var station= "B41"
+    var feed = 21;
+    var station = "B41"
 
-/*console.log(req.query)
-    var feed = parseInt(req.query.feed);
-    var station = req.query.station;*/
+    /*console.log(req.query)
+        var feed = parseInt(req.query.feed);
+        var station = req.query.station;*/
     console.log(feed)
     console.log(station)
 
-/*if(req.query.station) {*/
-mta.schedule("R43", 16).then(function (error, result) {
-    if(error) {
-        console.log(error)
-    } else {
+    /*if(req.query.station) {*/
+mta.schedule("R43", 16).then(function (result) {
   console.log(result);
- 
-  }
-   res.json(result)
+  res.json(result)
         });
-
-/*    }*/
 });
 
 
@@ -83,6 +77,3 @@ const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log(`Next Train listening on ${port}`);
-
-
-
